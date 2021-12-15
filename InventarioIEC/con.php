8b49e6715b4681c233c45488c25666d6a44bb227
Israel Ferreira <94846018+IsraFerreira@users.noreply.github.com>
2021-12-15 15:46:26 -0300

    Add files via upload

---
+++ InventarioIEC/con.php
@@ -0,0 +1,21 @@
+﻿
+<?php
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$dbname = "estoque";
+
+
+
+//criar a conexão
+$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+
+
+
+
+if(!$conn){
+	 echo "Falha na conexão com o banco de dados";
+}else{
+     echo "Conectado";
+}
+?>
